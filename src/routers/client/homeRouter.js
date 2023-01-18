12b2c87f05b3ba92c68f6277a7d3c9cba24e884c
unknown <173198977@qq.com>
2023-01-18 23:58:55 +0800

    首页页面接口开发完毕!

--- src/routers/client/homeRouter.js
+++ src/routers/client/homeRouter.js
@@ -10,6 +10,8 @@ router.get("/",(req,res)=>{
 })
 
 
+
+//下面的代码还有回头增加查询课程的功能
 //多级大纲列表查询
 router.get("/class_list",(req,resp)=>{
     resp.tool.execSQLTEMPAutoResponse(`
@@ -35,5 +37,43 @@ router.get("/ad",(req,resp)=>{
     WHERE is_home_page=?;
     `,[1],"首页广告轮播图查询成功！")
 })
+
+router.get("/home_course_list",(req,resp)=>{
+    resp.tool.execSQLTEMPAutoResponse(`
+    SELECT
+        t_courses.id,
+        t_courses.img_url,
+        t_courses.learning_time,
+        t_courses.stop_time,
+        t_courses.course_title,
+        t_courses.is_self_innovate,
+        t_teachers.name,
+        b.class_name AS title,
+        t_courses.price,
+        t_courses.special_course_categorys_id
+    FROM
+        t_courses
+    LEFT JOIN t_teachers ON t_courses.teacher_id = t_teachers.id
+    LEFT JOIN t_course_categorys ON t_courses.categorys_id = t_course_categorys.id
+    LEFT JOIN t_course_categorys AS a ON t_course_categorys.parent_id = a.id
+    LEFT JOIN t_course_categorys AS b ON a.parent_id = b.id 
+    WHERE
+        t_courses.is_special_home_show = 1 ;
+    `,[],"首页课程查询成功!")
+})
+
+router.get("/home_series_course",(req,resp)=>{
+    resp.tool.execSQLTEMPAutoResponse(`
+    SELECT
+        * 
+    FROM
+        t_series_courses 
+    WHERE
+        is_home_show = 1;
+    `,[],"首页系列课程查询成功!")
+})
+
+
+
 
 module.exports = router;
