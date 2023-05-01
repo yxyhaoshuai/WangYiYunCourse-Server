52ff3904a782f691a8409b464a9c2e4d1ab8466b
Yanxiuyu <173198977@qq.com>
2023-05-02 00:39:32 +0800

    开发了provider页面课程数量接口，用于做分页效果！

--- src/routers/client/providerDetail.js
+++ src/routers/client/providerDetail.js
@@ -25,10 +25,11 @@ router.get("/provider/course/list",(req,resp)=>{
     resp.tool.execSQLTEMPAutoResponse(`
         SELECT
             t_network_school.id AS schoolid,
-            t_courses.id AS courseid,
+            t_courses.id AS id,
             t_courses.course_title,
             t_courses.img_url,
             t_courses.price,
+            t_courses.course_intro,
             ROUND( avg( score ), 1 ) AS avg_score,
             t_courses.is_school_recommend 
         FROM
@@ -43,6 +44,51 @@ router.get("/provider/course/list",(req,resp)=>{
         ORDER BY
             ${method} desc 
         LIMIT ${(page_num - 1) * page_size}, ${page_size};
+    `,[id],"网校网课查询成功！")
+})
+
+
+//课程结果总数量查询成功，用于分页
+router.get("/provider/course/count/:id",(req,resp)=>{
+    const {id} =  req.params;
+    resp.tool.execSQLTEMPAutoResponse(`
+        SELECT
+            COUNT( t_network_school.id ) AS courseCount 
+        FROM
+            t_network_school
+        LEFT JOIN t_teachers ON t_network_school.id = t_teachers.school_id
+        LEFT JOIN t_courses ON t_teachers.id = t_courses.teacher_id 
+        WHERE
+            t_network_school.id = ?;
+    `,[id],"课程结果总数量查询成功！")
+})
+
+
+//provider页面中的主页中的推荐课程
+router.get("/provider/course/list/home",(req,resp)=>{
+    const {id} = req.query;
+    resp.tool.execSQLTEMPAutoResponse(`
+        SELECT
+            t_network_school.id AS schoolid,
+            t_courses.id AS id,
+            t_courses.course_title,
+            t_courses.img_url,
+            t_courses.price,
+            t_courses.course_intro,
+            ROUND( avg( score ), 1 ) AS avg_score,
+            t_courses.is_school_recommend
+        FROM
+            t_network_school
+        LEFT JOIN t_teachers ON t_network_school.id = t_teachers.school_id
+        LEFT JOIN t_courses ON t_teachers.id = t_courses.teacher_id
+        LEFT JOIN t_comment ON t_courses.id = t_comment.course_id 
+        GROUP BY
+            t_courses.id 
+        HAVING
+            t_network_school.id = ? and t_courses.is_school_recommend = 1
+        ORDER BY
+            create_time desc 
+        LIMIT 10;
     `,[id],"网校网课查询成功！")
 })
 
--- src/routers/client/teacherDetail.js
+++ src/routers/client/teacherDetail.js
@@ -17,6 +17,24 @@ router.get("/instructor/:id",(req,resp)=>{
     `,[id])
 })
 
+//instructor页
+router.get("/instructor/course/:id",(req,resp)=>{
+    const id = req.params.id;
+    resp.tool.execSQLTEMPAutoResponse(`
+    SELECT
+        t_courses.id,
+        t_courses.img_url,
+        t_courses.course_title,
+        t_courses.price,
+        t_teachers.name
+    FROM
+        t_teachers
+        LEFT JOIN t_courses ON t_teachers.id = t_courses.teacher_id 
+    WHERE
+        t_teachers.id = ?;
+    `,[id])
+})
+
 
 
 module.exports = router;
